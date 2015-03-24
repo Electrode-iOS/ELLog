f05abe969c5177830371301f350260051da2865a
Brandon Sneed <brandon@redf.net>
2015-03-24 09:54:13 -0700

    A little bit of refactoring.

---
+++ THGLog/LogDestinationBase.swift
@@ -0,0 +1,76 @@
+//
+//  LogDestinationBase.swift
+//  THGLog
+//
+//  Created by Brandon Sneed on 3/24/15.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
+//
+
+import Foundation
+
+
+/// Protocol that destination classes must support
+@objc(THGLogDestinationProtocol)
+public protocol LogDestinationProtocol: class {
+    /**
+    Sent to the destination when a log statement is executed.
+
+    :param: detail Detailed information about the log statement.
+    */
+    func log(detail: LogDetail)
+    /**
+    A unique identifier representing this destination.
+    */
+    var identifier: String { get }
+    /**
+    The levels that this destination actually records.  See LogLevel.
+    */
+    var level: UInt { get set }
+    /// Specifies whether this destination should show the caller information.
+    var showCaller: Bool { get set }
+    /// Specifies whether this destination should show the log level.
+    var showLogLevel: Bool { get set }
+    /// Specifies whether this destination should show the timestampe.
+    var showTimestamp: Bool { get set }
+}
+
+
+/// A struct describing a log message in detail.
+@objc(THGLogDetail)
+public class LogDetail {
+    /// The date at which the log call was made.  Note: This will never be an exact time, but approximate.
+    var date: NSDate? = nil
+    /// The message.
+    var message: String? = nil
+    /// The level at which this was logged.
+    var level: UInt? = .None
+    /// The function in which log was called.
+    var function: String? = nil
+    /// The filename in which log was called.
+    var filename: String? = nil
+    /// The line number which called log.
+    var line: UInt? = nil
+}
+
+
+@objc(THGLogDestinationBase)
+public class LogDestinationBase {
+    public init(level: LogLevel) {
+        self.level = level.rawValue
+    }
+
+    public init() {
+        level = LogLevel.Debug.rawValue
+    }
+
+    public func log(detail: LogDetail) {
+        // do nothing.
+    }
+
+    public var identifier: String = NSUUID().UUIDString
+    public var level: UInt
+
+    internal let dateFormatter: NSDateFormatter = NSThread.dateFormatter(dateFormat)
+    internal static let dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+    
+}
--- THGLog/Logger.swift
+++ THGLog/Logger.swift
@@ -8,10 +8,9 @@
 
 import Foundation
 
-struct TestPrintable : Printable {
-    var description: String { return "Testing Printable" }
-}
-
+/**
+LogLevel option flags.
+*/
 public struct LogLevel: RawOptionSetType, BooleanType, DebugPrintable {
     /// Logging disabled.
     public static var None: LogLevel       { return self(rawValue: 0) }
@@ -67,48 +66,6 @@ public struct LogLevel: RawOptionSetType, BooleanType, DebugPrintable {
     public var rawValue: UInt { return value }
     public static var allZeros: LogLevel { return self(rawValue: 0) }
 
-}
-
-/// Protocol that destination classes must support
-@objc(THGLogDestinationProtocol)
-public protocol LogDestinationProtocol: class {
-    /**
-    Sent to the destination when a log statement is executed.
-
-    :param: detail Detailed information about the log statement.
-    */
-    func log(detail: LogDetail)
-    /**
-    A unique identifier representing this destination.
-    */
-    var identifier: String { get }
-    /**
-    The levels that this destination actually records.  See LogLevel.
-    */
-    var level: UInt { get set }
-    /// Specifies whether this destination should show the caller information.
-    var showCaller: Bool { get set }
-    /// Specifies whether this destination should show the log level.
-    var showLogLevel: Bool { get set }
-    /// Specifies whether this destination should show the timestampe.
-    var showTimestamp: Bool { get set }
-}
-
-/// A struct describing a log message in detail.
-@objc(THGLogDetail)
-public class LogDetail {
-    /// The date at which the log call was made.  Note: This will never be an exact time, but approximate.
-    var date: NSDate? = nil
-    /// The message.
-    var message: String? = nil
-    /// The level at which this was logged.
-    var level: UInt? = .None
-    /// The function in which log was called.
-    var function: String? = nil
-    /// The filename in which log was called.
-    var filename: String? = nil
-    /// The line number which called log.
-    var line: UInt? = nil
 }
 
 
@@ -227,24 +184,3 @@ private func loggerDefault() -> Logger {
     return logger
 }
 
-@objc(THGLogDestinationBase)
-public class LogDestinationBase {
-    public init(level: LogLevel) {
-        self.level = level.rawValue
-    }
-
-    public init() {
-        level = LogLevel.Debug.rawValue
-    }
-
-    public func log(detail: LogDetail) {
-        // do nothing.
-    }
-
-    public var identifier: String = NSUUID().UUIDString
-    public var level: UInt
-
-    internal let dateFormatter: NSDateFormatter = NSThread.dateFormatter(dateFormat)
-    internal static let dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-
-}
