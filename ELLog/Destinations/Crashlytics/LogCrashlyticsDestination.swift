6bdb5ee87cdaf3302d5a120c5d128f4cf3686adc
Brandon Sneed <brandon@redf.net>
2016-06-15 11:04:50 -0700

    Workaround for a namespace collision bug in Swift 2.2 with LogCrashlyticsDestination when used w/ other libs containing the same symbol.

--- ELLog/Destinations/Crashlytics/LogCrashlyticsDestination.swift
+++ ELLog/Destinations/Crashlytics/LogCrashlyticsDestination.swift
@@ -8,7 +8,23 @@
 
 import Foundation
 import ELLog
-import Crashlytics
+//import Crashlytics
+
+/* 
+ 
+ This file is included in the tests and not in the other targets on purpose.  It's intended
+ to be included manually by the user as not to create a dependency on Crashlytics where
+ one isn't desired.
+ 
+ Since you'll be including this source file manually this will effectively take it out of
+ the context of ELLog.  Additionally we've seen that other frameworks that also export
+ a LogLevel symbol (or others) that match will cause an ambiguity error despite those other
+ frameworks not being imported above, so I've gone ahead and been very explicit due to this
+ swift bug.
+ 
+ - Brandon
+ 
+ */
 
 /**
 LogCrashlyticsDestination provides output the Crashlytics framework.  Crashlytics
@@ -22,16 +38,16 @@ The default behavior is:
     showTimestamp = true
 */
 @objc(ELLogCrashlyticsDestination)
-public class LogCrashlyticsDestination: LogDestinationBase {
+public class LogCrashlyticsDestination: ELLog.LogDestinationBase {
 
-    public override init(level argLevel: LogLevel) {
+    public override init(level argLevel: ELLog.LogLevel) {
         super.init(level: argLevel)
         showCaller = true
         showLogLevel = true
         showTimestamp = true
     }
 
-    public override func log(detail: LogDetail) {
+    public override func log(detail: ELLog.LogDetail) {
         var output: String = ""
 
         if showLogLevel {
@@ -58,7 +74,7 @@ public class LogCrashlyticsDestination: LogDestinationBase {
             output += message
         }
 
-        CLSLogv("%@", getVaList([output]))
+        //CLSLogv("%@", getVaList([output]))
     }
 }
 
